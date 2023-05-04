4c8407383beb4145a8593f14504817356c5be382
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-05-04 22:47:41 +0300

    add comment

--- provisioners/install-ansible.sh
+++ provisioners/install-ansible.sh
@@ -11,6 +11,7 @@ sudo yum -y install ansible
 # print Ansible version
 ansible --version
 
+# add temporary ip to hosts
 echo "192.168.10.10" | sudo tee -a /etc/ansible/hosts
 
 sudo ansible-playbook /vagrant/playbook.yml -vv
